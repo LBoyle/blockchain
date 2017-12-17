fa051c0510e892a6bcf98334eb3655e4a5cdf259
louis <louis_boyle@hotmail.com>
2017-12-17 20:02:43 +0000

    signing off

--- python/hackernoon/blockchain.py
+++ python/hackernoon/blockchain.py
@@ -1,15 +1,31 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        # Create genesis block
+        self.new_block(previous_hash=1, proof=100)
 
-    def new_block(self):
+    def new_block(self, proof, previous_hash=None):
         # Creates a new Block and adds it to the chain
-        pass
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            
+        }
 
-    def new_transaction(self):
+    def new_transaction(self, sender, recipient, amount):
         # Adds a new transaction to the list
-        pass
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+        # Index of the block that will hold this transaction
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
